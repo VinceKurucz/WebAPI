f1af94a9d26d9bafe00a67ebf8ab91675c3920ce
vince <vince@DESKTOP-RILJMGD>
2023-10-04 20:35:59 +0200

    Add project files.

---
+++ FSCTMM_2023241.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FSCTMM_2023241.Test
+{
+    public class Class1
+    {
+    }
+}
---
+++ FSCTMM_HFT_2023241.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FSCTMM_HFT_2023241.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MEGFOGOMCSINÁLNI");
+        }
+    }
+}
