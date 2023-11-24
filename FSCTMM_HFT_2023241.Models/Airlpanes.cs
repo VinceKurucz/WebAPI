2bbe852f81ae224138fed70ab08b6dce4453bfe2
vince <kuruczvince@stud.uni-obuda.hu>
2023-11-24 20:02:41 +0100

    Adatmodellek létrehozása

---
+++ FSCTMM_HFT_2023241.Models/Airlpanes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSCTMM_HFT_2023241.Models
+{
+    public class Airlpanes
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Speed { get; set;}
+
+
+        [NotMapped]
+        public virtual ICollection<Airports> Airports { get; set; }
+
+
+        [NotMapped]
+        public virtual ICollection<People> People { get; set; }
+
+    }
+}
---
+++ FSCTMM_HFT_2023241.Models/Airports.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSCTMM_HFT_2023241.Models
+{
+    public class Airports
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public int TakeOffPlatform { get; set; }
+
+
+        [NotMapped]
+        public virtual ICollection<Airlpanes> Airlpanes { get; set; }
+
+
+
+
+    }
+}
