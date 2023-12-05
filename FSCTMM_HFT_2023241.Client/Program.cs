3f73da2154126b17c9932f1bc023c4d6850b98ea
vince <kuruczvince@stud.uni-obuda.hu>
2023-12-05 17:57:58 +0100

    Console Menu

--- FSCTMM_HFT_2023241.Client/Program.cs
+++ FSCTMM_HFT_2023241.Client/Program.cs
@@ -1,18 +1,66 @@
-﻿using System;
+﻿using ConsoleTools;
+using FSCTMM_HFT_2023241.Models;
+using System;
 using System.Linq;
 using System.Net;
 
 namespace FSCTMM_HFT_2023241.Client
 {
-    internal class Program
+     class Program
     {
         static void Main(string[] args)
         {
+
+
             ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true; //Ezt az SSL exceptionok miatt
 
 
             RestService rest = new RestService("http://localhost:18364/", "name");
+            CrudServices crud = new CrudServices(rest);
+            NonCrudService nonCrud = new NonCrudService(rest);
 
+
+
+
+            var AirplanesMenu = new ConsoleMenu(args, level: 1)
+               .Add("List", () => crud.List<Airlpanes>())
+               .Add("Create", () => crud.Create<Airlpanes>())
+               .Add("Update", () => crud.Update<Airlpanes>())
+               .Add("Delete", () => crud.Delete<Airlpanes>())
+               .Add("Exit", ConsoleMenu.Close);
+
+            var CrewMenu = new ConsoleMenu(args, level: 1)
+               .Add("List", () => crud.List<Crew>())
+               .Add("Create", () => crud.Create<Crew>())
+               .Add("Update", () => crud.Update<Crew>())
+               .Add("Delete", () => crud.Delete<Crew>())
+               .Add("Exit", ConsoleMenu.Close);
+
+            var AirportMenu = new ConsoleMenu(args, level: 1)
+               .Add("List", () => crud.List<Airports>())
+               .Add("Create", () => crud.Create<Airports>())
+               .Add("Update", () => crud.Update<Airports>())
+               .Add("Delete", () => crud.Delete<Airports>())
+               .Add("Exit", ConsoleMenu.Close);
+
+            var StatsSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("Averege seats", () => nonCrud.avgseats())
+                .Add("CrewWithBigPlaneSpeed", () => nonCrud.CrewWithBigPlaneSpeed())
+                .Add("AirportWithBestCrew", () => nonCrud.AirportWithBestCrew())
+                .Add("BigAndGoodPlanesAirports", () => nonCrud.BigAndGoodPlanesAirports())
+                .Add("BigAirportsFastPlanes", () => nonCrud.BigAirportsFastPlanes())
+                .Add("Exit", ConsoleMenu.Close);
+
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Airplane", () => AirplanesMenu.Show())
+                .Add("Crew", () => CrewMenu.Show())
+                .Add("Airport", () => AirportMenu.Show())
+                .Add("NonCrud", () => StatsSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
+
+            menu.Show();
+
+            Console.WriteLine("asd");
         }
     }
 }
--- FSCTMM_HFT_2023241.Endpoint/Startup.cs
+++ FSCTMM_HFT_2023241.Endpoint/Startup.cs
@@ -21,12 +21,12 @@ namespace FSCTMM_HFT_2023241.Endpoint
     public class Startup
     {
 
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
 
-        public IConfiguration Configuration { get; }
+        //public IConfiguration Configuration { get; }
 
 
         // This method gets called by the runtime. Use this method to add services to the container.
